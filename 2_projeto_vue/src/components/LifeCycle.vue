f0dfbb6db62f24e34c6ffa640c567ffac5d9327c
Igor Santos <igorsantos888@gmail.com>
2022-10-19 10:05:37 -0300

    Criado o Life Cycle e implementado alguns components no App.Vue

---
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default {
+  name: "LifeCycle",
+  data() {
+    return {
+      nome: "Ainda não sei",
+    };
+  },
+  created() {
+    setTimeout(() => {
+        this.nome = 'Igor'
+    }, 2000)
+  },
+  mounted() {
+    setTimeout(() => {
+        this.nome = 'Julia'
+    }, 4000)
+  }
+};
+</script>
