2d703aca63b4332a08ddfee81223111bbfa85cce
Igor Santos <igorsantos888@gmail.com>
2022-10-19 09:14:19 -0300

    Instalado o Vue via CDN. Feito o primeiros códigos básicos de aprendizagem como Data Binding e input.

---
+++ 1_Instalacao_Vue/src/app.js
@@ -0,0 +1,23 @@
+const MyNameApp = {
+    data() {
+        return {
+            name: '',
+            age: '',
+            input_name: '',
+            input_age: '',
+        }
+    },
+    methods: {
+        submitForm(e) {
+            e.preventDefault()
+
+            console.log(this.input_name)
+
+            this.name = this.input_name
+            this.age = this.input_age
+
+        }
+    }
+}
+
+Vue.createApp(MyNameApp).mount('#app')
