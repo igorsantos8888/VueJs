8e2f96f8b181d3ebe04632578d46d910d46804c5
Igor Santos <igorsantos888@gmail.com>
2022-10-19 10:35:33 -0300

    criado os components Form, Submit e Input para entender a Hierarquia de componentes

---
+++ 2_projeto_vue/src/components/Info.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Estou trabalhando no momento.</p>
+    <p>Utilizo as seguintes tecnologias</p>
+    <ul>
+      <li>JavaScript</li>
+      <li>PHP</li>
+      <li>Python</li>
+      <li>VueJs</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Info",
+};
+</script>
---
+++ 2_projeto_vue/src/components/Pessoa.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Está é a descrição da pessoa:  {{nome}}</h2>
+        <Info/>
+        <Form/>
+    </div>
+</template>
+
+<script>
+import Info from './Info.vue';
+import Form from './Form.vue';
+    export default {
+    name: "Pessoa",
+    data() {
+        return {
+            nome: "Igor"
+        };
+    },
+    components: { Info, Form }
+}
+</script>
